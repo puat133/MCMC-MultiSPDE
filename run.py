e829f776c97bc5e11a76988ad312a8018984a9b6
puat133 <puat133@gmail.com>
2019-07-16 15:32:19 +0300

    update

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import mcmc.simulation as s
+import mcmc.plotting as p
 
-sim = s.Simulation(n_samples = 1000,n = 2**6,beta = 2e-1,num = 2**8,
+sim = s.Simulation(n_samples = 10000,n = 2**6,beta = 2e-1,num = 2**8,
                 kappa = 1e17,sigma_u = 5e6,sigma_v = 10,evaluation_interval = 100,
-                seed=1,burn_percentage = 5.0);s
+                seed=1,burn_percentage = 5.0)
 sim.run()
 sim.analyze()
+p.plotResult(sim)
