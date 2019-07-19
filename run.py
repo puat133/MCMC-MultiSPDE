d1f03e9772546fc7e73e49142a02a9ab70bc8491
puat133 <puat133@gmail.com>
2019-07-19 15:08:49 +0300

    update

--- run.py
+++ run.py
@@ -3,16 +3,19 @@ import mcmc.simulation as s
 import mcmc.plotting as p
 import matplotlib.pyplot as plt
 #%%
-sim = s.Simulation(n_layers=2,n_samples = 10000,n = 2**4,beta = 2e-1,num = 2**8,
+n = 2**5
+sim = s.Simulation(n_layers=2,n_samples = 10000,n = n,beta = 2e-1,num = 2**8,
                 kappa = 1e17,sigma_u = 5e6,sigma_v = 10,evaluation_interval = 100,
-                seed=1,burn_percentage = 5.0)
+                seed=1,burn_percentage = 50.0)
+
+sim.pcn.aggresiveness = 0.1
 sim.run()
 sim.analyze()
 p.plotResult(sim)
 # sim.analyze()
 # p.plotResult(sim)
 
-#%%
+
 
 
 #%%
