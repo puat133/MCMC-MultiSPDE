1e366b4aea6620d55c28d54fafb35c689d0089a3
puat133 <puat133@gmail.com>
2019-07-26 12:39:04 +0300

    add .copy()

--- mcmc/layer.py
+++ mcmc/layer.py
@@ -5,35 +5,35 @@ import mcmc.fourier as fourier
 import mcmc.L as L
 import mcmc.pCN as pCN
 
-# L_matrix_type = nb.deferred_type()
-# L_matrix_type.define(L.Lmatrix.class_type.instance_type)  
-# pCN_type = nb.deferred_type()
-# pCN_type.define(pCN.pCN.class_type.instance_type)
-# fourier_type = nb.deferred_type()
-# fourier_type.define(fourier.FourierAnalysis.class_type.instance_type)     
-# spec = [
-#     ('is_stationary',nb.boolean),
-#     ('sqrt_beta',nb.float64),
-#     ('order_number',nb.int64),
-#     ('n_samples',nb.int64),
-#     ('pcn',pCN_type),
-#     ('i_record',nb.int64),
-#     ('stdev',nb.complex128[:]),
-#     ('samples_history',nb.complex128[:,:]),
-#     ('current_sample',nb.complex128[:]),
-#     ('current_sample_symmetrized',nb.complex128[:]),
-#     ('current_sample_scaled_norm',nb.float64),
-#     ('current_log_L_det',nb.float64),
-#     ('new_sample',nb.complex128[:]),
-#     ('new_sample_symmetrized',nb.complex128[:]),
-#     ('new_sample_scaled_norm',nb.float64),
-#     ('new_log_L_det',nb.float64),
-#     ('LMat',L_matrix_type),
+L_matrix_type = nb.deferred_type()
+L_matrix_type.define(L.Lmatrix.class_type.instance_type)  
+pCN_type = nb.deferred_type()
+pCN_type.define(pCN.pCN.class_type.instance_type)
+fourier_type = nb.deferred_type()
+fourier_type.define(fourier.FourierAnalysis.class_type.instance_type)     
+spec = [
+    ('is_stationary',nb.boolean),
+    ('sqrt_beta',nb.float64),
+    ('order_number',nb.int64),
+    ('n_samples',nb.int64),
+    ('pcn',pCN_type),
+    ('i_record',nb.int64),
+    ('stdev',nb.complex128[:]),
+    ('samples_history',nb.complex128[:,:]),
+    ('current_sample',nb.complex128[:]),
+    ('current_sample_symmetrized',nb.complex128[:]),
+    ('current_sample_scaled_norm',nb.float64),
+    ('current_log_L_det',nb.float64),
+    ('new_sample',nb.complex128[:]),
+    ('new_sample_symmetrized',nb.complex128[:]),
+    ('new_sample_scaled_norm',nb.float64),
+    ('new_log_L_det',nb.float64),
+    ('LMat',L_matrix_type),
     
     
-# ]
+]
 
-# @nb.jitclass(spec)
+@nb.jitclass(spec)
 class Layer():
     def __init__(self,is_stationary,sqrt_beta,order_number,n_samples,pcn,init_sample):
         self.is_stationary = is_stationary
@@ -68,8 +68,8 @@ class Layer():
             self.new_sample_scaled_norm = 0
             self.new_log_L_det = 0
             #numba need this initialization. otherwise it will not compile
-            self.current_sample = init_sample
-            self.current_sample_symmetrized = self.new_sample_symmetrized
+            self.current_sample = init_sample.copy()
+            self.current_sample_symmetrized = self.new_sample_symmetrized.copy()
             self.current_sample_scaled_norm = 0
             self.current_log_L_det = 0
             
@@ -83,8 +83,8 @@ class Layer():
             self.new_sample_scaled_norm = util.norm2(self.LMat.current_L@self.new_sample_symmetrized)#ToDO: Modify this
             self.new_log_L_det = self.LMat.logDet()#ToDO: Modify this
             # #numba need this initialization. otherwise it will not compile
-            self.current_sample = init_sample
-            self.current_sample_symmetrized = self.new_sample_symmetrized
+            self.current_sample = init_sample.copy()
+            self.current_sample_symmetrized = self.new_sample_symmetrized.copy()
             self.current_sample_scaled_norm = self.new_sample_scaled_norm
             self.current_log_L_det = self.new_log_L_det   
             
@@ -121,7 +121,7 @@ class Layer():
                     w = np.random.randn()
                 elif element_index < self.pcn.fourier.fourier_basis_number:
                     w = (np.random.randn()+1j*np.random.randn())/np.sqrt(2)
-                self.new_sample = self.current_sample
+                self.new_sample = self.current_sample.copy()
                 self.new_sample[element_index] = self.pcn.betaZ*self.current_sample[element_index] + self.pcn.beta*self.stdev[element_index]*w
                 # self.new_sample_symmetrized = self.pcn.random_gen.symmetrize(self.new_sample)
                 
@@ -132,8 +132,8 @@ class Layer():
         self.i_record += 1
     
     def update_current_sample(self):
-        self.current_sample = self.new_sample
-        self.current_sample_symmetrized = self.new_sample_symmetrized
+        self.current_sample = self.new_sample.copy()
+        self.current_sample_symmetrized = self.new_sample_symmetrized.copy()
         self.current_sample_scaled_norm = self.new_sample_scaled_norm
         self.current_log_L_det = self.new_log_L_det
 
