602f8c409f4089ac6321d29502d33a8b1c8410b6
puat133 <puat133@gmail.com>
2019-08-01 13:47:03 +0300

    new pCN algorithm for multi layer.

--- mcmc/pCN.py
+++ mcmc/pCN.py
@@ -8,28 +8,28 @@ import mcmc.measurement as meas
 
 
 
-# Rand_gen_type = nb.deferred_type()
-# Rand_gen_type.define(randomGenerator.RandomGenerator.class_type.instance_type)
-# meas_type = nb.deferred_type()
-# meas_type.define(meas.Measurement.class_type.instance_type)
-# fourier_type = nb.deferred_type()
-# fourier_type.define(fourier.FourierAnalysis.class_type.instance_type)    
-# spec = [
-#     ('n_layers',nb.int64),
-#     ('beta',nb.float64),
-#     ('betaZ',nb.float64),
-#     ('random_gen',Rand_gen_type),
-#     ('measurement',meas_type),
-#     ('fourier',fourier_type),
-#     ('H',nb.complex128[:,:]),
-#     ('yBar',nb.float64[:]),
-#     ('gibbs_step',nb.int64),
-#     ('aggresiveness',nb.float64),
-#     ('target_acceptance_rate',nb.float64),
-#     ('beta_feedback_gain',nb.float64),
-# ]
+Rand_gen_type = nb.deferred_type()
+Rand_gen_type.define(randomGenerator.RandomGenerator.class_type.instance_type)
+meas_type = nb.deferred_type()
+meas_type.define(meas.Measurement.class_type.instance_type)
+fourier_type = nb.deferred_type()
+fourier_type.define(fourier.FourierAnalysis.class_type.instance_type)    
+spec = [
+    ('n_layers',nb.int64),
+    ('beta',nb.float64),
+    ('betaZ',nb.float64),
+    ('random_gen',Rand_gen_type),
+    ('measurement',meas_type),
+    ('fourier',fourier_type),
+    ('H',nb.complex128[:,:]),
+    ('yBar',nb.float64[:]),
+    ('gibbs_step',nb.int64),
+    ('aggresiveness',nb.float64),
+    ('target_acceptance_rate',nb.float64),
+    ('beta_feedback_gain',nb.float64),
+]
 
-# @nb.jitclass(spec)
+@nb.jitclass(spec)
 class pCN():
     def __init__(self,n_layers,rg,measurement,f,beta=1):
         self.n_layers = n_layers
@@ -61,50 +61,92 @@ class pCN():
         self.set_beta(np.min(np.array([(1+self.aggresiveness)*self.beta,1],dtype=np.float64)))
     
     def less_aggresive(self):
-        self.set_beta(np.min(np.array([(1-self.aggresiveness)*self.beta,1e-5],dtype=np.float64)))
+        self.set_beta(np.min(np.array([(1-self.aggresiveness)*self.beta,1e-10],dtype=np.float64)))
 
     def set_beta(self,newBeta):
         self.beta = newBeta
         self.betaZ = np.sqrt(1-newBeta**2)
         
     def oneStep(self,Layers):
-        logRatio = 0.0
-        for i in range(self.n_layers):
-        # i = int(self.gibbs_step//len(Layers))
-            
-            Layers[i].sample()
-            # new_sample = Layers[i].new_sample
-            if i> 0:
-                Layers[i].LMat.construct_from(Layers[i-1].new_sample)
-                Layers[i].new_log_L_det = np.linalg.slogdet(Layers[i].LMat.latest_computed_L)[1]
-                # if i < self.n_layers - 1 :
-                #     Layers[i].current_sample_scaled_norm = util.norm2(Layers[i].LMat.current_L@Layers[i].current_sample_symmetrized)
-                # else:
-                Layers[i].current_sample_scaled_norm = util.norm2(Layers[i].LMat.current_L@Layers[i].new_sample_symmetrized)
-                Layers[i].new_sample_scaled_norm = util.norm2(Layers[i].LMat.latest_computed_L@Layers[i].new_sample_symmetrized)
-
-                logRatio += 0.5*(Layers[i].current_sample_scaled_norm-Layers[i].new_sample_scaled_norm)
-                logRatio += (Layers[i].new_log_L_det-Layers[i].current_log_L_det)
-            else:
-                #TODO: Check whether 0.5 factor should be added below
-                Layers[i].new_sample_scaled_norm = util.norm2(Layers[i].new_sample/Layers[i].stdev)
-                logRatio += (Layers[i].current_sample_scaled_norm-Layers[i].new_sample_scaled_norm)
-            
-        if logRatio>np.log(np.random.rand()):
-            for i in range(self.n_layers):
-                Layers[i].update_current_sample()
-                if not Layers[i].is_stationary:
-                    Layers[i].LMat.set_current_L_to_latest()
-                
-            accepted = 1
-        else:
-            accepted=0
-        # self.gibbs_step +=1
         
-        for i in range(self.n_layers):
-            Layers[i].record_sample()
+        accepted = 0
+        for i in range(self.n_layers-1,0,-1):#do it from the back
+        # i = int(self.gibbs_step//len(Layers))
+            logRatio = 0.0
+            if i == self.n_layers-1:    
+                Layers[i].sample()
+            Layers[i-1].sample()
+            #Layer i
+            Layers[i].LMat.construct_from(Layers[i-1].new_sample)
+            Layers[i].new_log_L_det = np.linalg.slogdet(Layers[i].LMat.latest_computed_L)[1]
+            
+            Layers[i].current_sample_scaled_norm = util.norm2(Layers[i].LMat.current_L@Layers[i].new_sample_symmetrized)
+            # assert Layers[i].current_sample_scaled_norm != np.nan
+            Layers[i].new_sample_scaled_norm = util.norm2(Layers[i].LMat.latest_computed_L@Layers[i].new_sample_symmetrized)
+            # assert Layers[i].new_sample_scaled_norm != np.nan
+
+            logRatio += 0.5*(Layers[i].current_sample_scaled_norm-Layers[i].new_sample_scaled_norm)
+            logRatio += (Layers[i].new_log_L_det-Layers[i].current_log_L_det)
+            
+            #Layer i-1
+            if i-1>0:
+                Layers[i-1].new_sample_scaled_norm = util.norm2(Layers[i-1].LMat.current_L@Layers[i-1].new_sample_symmetrized)
+            else:
+                Layers[i-1].new_sample_scaled_norm = util.norm2(Layers[i-1].new_sample/Layers[i-1].stdev)
+                    
+            logRatio += (Layers[i-1].current_sample_scaled_norm-Layers[i-1].new_sample_scaled_norm)
+            if logRatio>np.log(np.random.rand()):
+                for i in range(self.n_layers):
+                    Layers[i].update_current_sample()
+                    if not Layers[i].is_stationary:
+                        Layers[i].LMat.set_current_L_to_latest()
+                    
+                accepted += 1
+        
+            
+            for i in range(self.n_layers):
+                Layers[i].record_sample()
 
         return accepted
+    
+    # def oneStep(self,Layers):
+    #     logRatio = 0.0
+    #     for i in range(self.n_layers):
+    #     # i = int(self.gibbs_step//len(Layers))
+            
+    #         Layers[i].sample()
+    #         # new_sample = Layers[i].new_sample
+    #         if i> 0:
+    #             Layers[i].LMat.construct_from(Layers[i-1].new_sample)
+    #             Layers[i].new_log_L_det = np.linalg.slogdet(Layers[i].LMat.latest_computed_L)[1]
+    #             # if i < self.n_layers - 1 :
+    #             #     Layers[i].current_sample_scaled_norm = util.norm2(Layers[i].LMat.current_L@Layers[i].current_sample_symmetrized)
+    #             # else:
+    #             Layers[i].current_sample_scaled_norm = util.norm2(Layers[i].LMat.current_L@Layers[i].new_sample_symmetrized)
+    #             Layers[i].new_sample_scaled_norm = util.norm2(Layers[i].LMat.latest_computed_L@Layers[i].new_sample_symmetrized)
+
+    #             logRatio += 0.5*(Layers[i].current_sample_scaled_norm-Layers[i].new_sample_scaled_norm)
+    #             logRatio += (Layers[i].new_log_L_det-Layers[i].current_log_L_det)
+    #         else:
+    #             #TODO: Check whether 0.5 factor should be added below
+    #             Layers[i].new_sample_scaled_norm = util.norm2(Layers[i].new_sample/Layers[i].stdev)
+    #             logRatio += (Layers[i].current_sample_scaled_norm-Layers[i].new_sample_scaled_norm)
+            
+    #     if logRatio>np.log(np.random.rand()):
+    #         for i in range(self.n_layers):
+    #             Layers[i].update_current_sample()
+    #             if not Layers[i].is_stationary:
+    #                 Layers[i].LMat.set_current_L_to_latest()
+                
+    #         accepted = 1
+    #     else:
+    #         accepted=0
+    #     # self.gibbs_step +=1
+        
+    #     for i in range(self.n_layers):
+    #         Layers[i].record_sample()
+
+    #     return accepted
 
     def one_step_one_element(self,Layers,element_index):
         logRatio = 0.0
