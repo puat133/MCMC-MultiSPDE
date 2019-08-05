339ee6f289fd4ef07224182fee811b19374f2af4
puat133 <puat133@gmail.com>
2019-08-05 16:35:22 +0300

    update

--- cython/setup.py
+++ cython/setup.py
@@ -5,10 +5,6 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 setup(
-    ext_modules = cythonize(Extension("radon_matrix",sources=["radon_matrix.pyx"],
-    extra_link_args=["-O3","-fopenmp"],
-    language="c++",
-    extra_compile_args=["-O3", "-march=native","-fopenmp"],
-    include_dirs=[np.get_include()]))
+    ext_modules = cythonize(Extension("matrices",sources=["matrices.pyx"],extra_link_args=["-O3","-fopenmp"],language="c++",extra_compile_args=["-O3", "-march=native","-fopenmp"]))
     
 )
