39b2873614d03e7e2fe2f980c278d994d8a912ee
puat133 <puat133@gmail.com>
2019-08-21 16:40:02 +0300

    update

--- mcmc/autocorr.py
+++ mcmc/autocorr.py
@@ -18,21 +18,17 @@ jitParallel = nb.jit(fastmath=FASTMATH,cache=CACHE,parallel=PARALLEL)
 #
 #    return np.array(corr)
 
-@jitParallel
+
 def autocorr(uHalfHistory):
     length = uHalfHistory.shape[0]
     nFourier = uHalfHistory.shape[1]
-    uHalfMean = np.mean(uHalfHistory,0)
-    uHalfNormalized = uHalfHistory - uHalfMean
-    uHalfVar = np.var(uHalfHistory,0)
 #    lags = np.arange(length)
     xcorr = np.zeros((nFourier,length))
     for i in nb.prange(nFourier):
-        for j in nb.prange(length):
-            if j==0:
-                xcorr[i,j]  = 1
-            else:
-                xcorr[i,j] = np.sum(uHalfNormalized[j:,i]*uHalfNormalized[:-j,i].conj()).real/(length*uHalfVar[i])
+        temp = np.correlate(uHalfHistory[:,i],uHalfHistory[:,i],mode='full');
+        xcorr[i,:] = temp[temp.size//2:]
+        xcorr[i,:] = xcorr[i,:]*xcorr[i,:]
+        xcorr[i,:] = xcorr[i,:]/xcorr[i,0]
         
     return xcorr
 
