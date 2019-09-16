43efaf38d6bafec768bd7c35f2212ebae8ad8242
puat133 <puat133@gmail.com>
2019-09-16 09:05:47 +0300

    update

---
+++ cupy_test.py
@@ -0,0 +1,6 @@
+import cupy as cp
+import numpy as np
+
+rand_array = cp.random.randn(100,dtype=np.float32)
+print(rand_array)
+
---
+++ mcmc/twoDtest.py
@@ -0,0 +1,8 @@
+#%%
+import matplotlib.pyplot as plt
+import mcmc.image as im
+import mcmc.plotting as p
+import numpy as np
+import scipy.linalg as sla
+import scipy.special as ssp
+import mcmc.util_2D as u2
