f9a3341485cb7696e4df91e1e4a0be2a16e1d3d6
puat133 <puat133@gmail.com>
2019-09-18 14:09:40 +0300

    delete twoDtest.py

--- mcmc/twoDtest.py
+++
@@ -1,8 +0,0 @@
-#%%
-import matplotlib.pyplot as plt
-import mcmc.image as im
-import mcmc.plotting as p
-import numpy as np
-import scipy.linalg as sla
-import scipy.special as ssp
-import mcmc.util_2D as u2
